a9131a9d31eaba0eb5b077959f9a2aa75c6937e6
Guixaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2025-06-04 21:27:13 -0300

    Aprendendo mais sobre express, routes e etc...

---
+++ treino/Ambiente_treino_dev.md
@@ -0,0 +1,8 @@
+1 - Fazer o ambiente de desenvolvimento dev e configurações específicas que estão na pasta doc
+
+2 - Baixar a extensão REST CLIENT do VScode, ele vai ser o nosso POSTMEN ou INSOMIA
+
+3 - Criar um arquivo.http para utilizar o REST CLIENT! Lembre -se: requisições HTTP feitas pelo REST CLIENT 
+atua do lado do cliente, simulando chamadas que um navegador, um aplicativo ou um sistema externo fariam para um servidor.
+
+4 - MÃO NA MASSA!
--- treino/express.ts
+++ treino/express.ts
@@ -1,5 +1,5 @@
 /* IMPORTANDO A LIB Lib EXPRESS */
-import express,{Express,Request,Response} from 'express'; 
+import express,{Express,Request,response,Response} from 'express'; 
 
 /*
  QUANDO COLOCAMOS {}, TODAS AS FUNÇÕES, MÓDULOS, ATRIBUTOS, CLASSES QUE ESTÃO DENTRO DE CHAVES, ESTAMOS IMPORTANDO
@@ -52,6 +52,7 @@ app.get('/', async (req:Request,res:Response)=>{
     }
 })
 
+
 /*                                        esse cara é o nosso callback
     app.get('caminho_rota', (req: requisição do cliente, res: resposta do servidor) ) 
     
@@ -62,11 +63,120 @@ app.get('/', async (req:Request,res:Response)=>{
     status(código_status_http) 
 
     res.send('mensagem de retorno') -> do nosso objeto Response, estamos utilizando um
-    método
+    método, ou seja, no Express.js é usado para enviar uma resposta ao cliente que fez a
+    requisição. Ele faz parte do objeto Response (res) e pode enviar diferentes tipos de conteúdo, 
+    como strings, objetos JSON, buffers ou até mesmo arquivos.
+*/
+
+app.post('/rescebendo_mensagem/:mensagem', async (req:Request,res:Response) => {
+
+    try{
+        
+        const mensagem:string = req.params.mensagem;
+
+        res.status(200).send(`Servidor rescebeu a mensgem da requisição post: ${mensagem}`);
+    
+    }catch(e){
+
+    };
+
+});
+
+
+/* 
+
+    Basicamente vai funcionar da mesma forma acima, porém com requisições tipos post!
+    utilize o postman, isonimia ou a extensão REST CLIENT para fazer a requisição
+    POST
+
+    o req.params.mensagem -> é uma tributo específico do objeto Request que capita 
+    uma parâmetro declarado na URL, esse parâmetro é declarado com : 
+
 */
 
 
+// Simulando um banco de dados em memória
 
+// criando um tipo, funciona igual ao type do C, basicamente definimos um tipo de dados para posterior mente ser criada
+type Database_Pessoa= {
+    id:number,
+    nome?:string, /* onde tem ? significa que são variáveis que podem ou não ser utilizadas */
+    ativo:boolean
+
+}
+
+
+let database: { [key: number]: Database_Pessoa } = {
+    1: { id: 1, nome: "Carlos", ativo: true },
+    2: { id: 2, ativo: false }
+};
+  
+
+/* 
+
+    EXPLICAÇÃO DETALHADA DA VARIÁVEL DATABASE:
+
+    let database é uma variável que pode ser modificada e mudada ao longo do bloco código
+
+    diferente do const, que é unico, fixo, costante naquele bloco de código 
+
+    Esse formato { [key: number]: Database_Pessoa } é chamado de Index Signature no TypeScript
+    
+    Ele permite criar objetos onde: 
+        
+        - Cada chave (key) é um número. 
+        - Cada valor segue o tipo Database_Pessoa.
+
+    É uma forma dinâmica de armazenar múltiplos objetos do tipo Database_Pessoa,
+    onde as chaves representam identificadores únicos (IDs).
+
+
+    Agora: 
+
+        {
+            1: { id: 1, nome: "Carlos", ativo: true },
+            2: { id: 2, ativo: false }
+        };
+
+    É um objeto em TypeScript onde:
+
+        As chaves (1, 2) são números que representam identificadores únicos.
+
+        Os valores associados a essas chaves são objetos, que contêm dados sobre usuários.
+
+    Resumindo: 
+
+        Criamos um objeto de objetos. Ou seja, database é um objeto principal, onde cada chave numérica
+        (como 1, 2, 3) aponta para outro objeto que contém informações específicas.
+
+*/
+
+
+app.put('/atualizar_Moc/:id', async(req:Request,res:Response) => {
+
+    try{
+
+        const id:number = Number(parseInt(req.params.id));
+        const novosDados: Partial<Database_Pessoa> = req.body; // Permite atualizar parcialmente o nosso banco de dados em memória
+
+        // Verifica se o ID existe
+        if(!database[id]){
+            res.status(404).json({erro:'Registro não encontrado no banco de dados em memória'});
+        }
+
+        // Atualiza os dados mantendo os valores existentes
+
+        database[id] = {...database[id], ...novosDados}; // esse objeto está adquirindo todos os atributos de database[id] e novosDados
+
+        res.status(200).json({mensagem:'Dados Atualizados',dados:database})
+    
+    
+    }catch(e){
+        console.log(`Deu erro ao atualizar algum dado do nosso MOC: ${e}`);
+        res.status(500).json({ erro: 'Erro interno no servidor' });
+    };
+
+});
 
 
 /*FAZENDO O NOSSO SERVIDOR RODAR LOCALMENTE NA PORTA: 8080 */
@@ -75,4 +185,5 @@ app.get('/', async (req:Request,res:Response)=>{
 app.listen(8080, ():void =>{
 
     console.log('Servidor rodando na porta: 8080')
+    
 });
